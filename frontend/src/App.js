33144d553944041b9af615871dedcf430a5bb010
EshaBachuwar <eshabachuwar09@gmail.com>
2023-08-08 17:36:39 +0530

    Added the booking form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import BookingForm from './components/BookingForm';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [calendarDate, setCalendarDate] = useState('');
+  // setCalendarDate = (date) => {
+  //   this.setState({ calendarDate: date });
+  // };
+  // const setCalendarDate = this.setCalendarDate;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/booking"
+            element={
+              <BookingForm
+                date={12 / 8 / 2025}
+                updateCalender={setCalendarDate}
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  // { signOut, loadMyBookings, user }
+  return (
+    <nav className="flex justify-end p-5 bg-cyan-800 text-white">
+      <div className="flex">
+        <div>
+          <Link to="/" className="p-2 font-bold text-xl">
+            Book a Room
+          </Link>
+        </div>
+        <div>
+          <Link to="/" className="p-2 font-bold text-xl">
+            My Bookings
+          </Link>
+        </div>
+        <div>
+          <a className="p-2 font-bold text-xl">Logout</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
